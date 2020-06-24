c365afb15811888acbcfb5097de24aaaef2147ef
James S <Jspalding@users.noreply.github.com>
2020-06-24 23:34:55 +0100

    Updates

---
+++ precache-manifest.8dadc21dff95cd2a5ed6c5129b73561c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68a77cd03e5e2229ecce8c6bf9311697",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "3379d6a5322477b4a881",
+    "url": "/covid-19-tracker/static/css/main.3a3ab36c.chunk.css"
+  },
+  {
+    "revision": "ea8985ea3e5235fd4a63",
+    "url": "/covid-19-tracker/static/js/2.2ff0257d.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/covid-19-tracker/static/js/2.2ff0257d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3379d6a5322477b4a881",
+    "url": "/covid-19-tracker/static/js/main.c7f6fc5f.chunk.js"
+  },
+  {
+    "revision": "3d1c3a839b5135420af2",
+    "url": "/covid-19-tracker/static/js/runtime-main.94eb6b52.js"
+  }
+]);
