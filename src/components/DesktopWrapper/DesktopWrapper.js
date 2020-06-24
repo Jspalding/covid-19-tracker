ce1eb7246da7aec58448eb4e2f5da11517c961b6
James S <Jspalding@users.noreply.github.com>
2020-06-24 19:48:41 +0100

    Replaced table component with react-table component

--- src/components/DesktopWrapper/DesktopWrapper.js
+++ src/components/DesktopWrapper/DesktopWrapper.js
@@ -1,74 +1,111 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useMemo } from 'react'
 import './DesktopWrapper.css';
 
 import axios from 'axios';
 import { SUMMARY_URL } from '../../util/api.config';
 
 import HeadDataCard from "./HeadDataCard/HeadDataCard";
-import DisplayTable from "./DisplayTable/DisplayTable";
+import DisplayDataTable from "./DisplayTable/DisplayDataTable";
 
 
 const DesktopWrapper = props => {
 
-    const [summaryData, setSummaryData] = useState({ Countries: [], Global: [] });
-    const [isLoading, setIsLoading] = useState(false);
+  const [summaryData, setSummaryData] = useState({ Countries: [], Global: [] });
+  const [isLoading, setIsLoading] = useState(false);
 
-    useEffect(() => {
-        setIsLoading(true);
+  useEffect(() => {
+    setIsLoading(true);
 
-        const fetchData = async () => {
-            const result = await axios(
-                 `${SUMMARY_URL}`
-             );
-            setSummaryData(result.data);
-            setIsLoading(false);
-        };
+    const fetchData = async () => {
+      const result = await axios(
+        `${SUMMARY_URL}`
+      );
+      setSummaryData(result.data);
+      setIsLoading(false);
+    };
 
-        fetchData();
-    }, []);
+    fetchData();
+  }, []);
 
-    const formatter = new Intl.NumberFormat();
+  const formatter = new Intl.NumberFormat();
 
-    return (
-        <div className="desktop-wrapper bg-gray-200">
-            <div className="grid grid-cols-6 gap-3">
-                <HeadDataCard
-                    cardTitle="Covid Cases"
-                    cardTitleColour="blue"
-                    mainData={summaryData.Global.TotalConfirmed}
-                    secondaryData={summaryData.Global.NewConfirmed}
-                    isLoading={isLoading}
-                    format={formatter}
-                />
-                <HeadDataCard
-                    cardTitle="Deaths"
-                    cardTitleColour="red"
-                    mainData={summaryData.Global.TotalDeaths}
-                    secondaryData={summaryData.Global.NewDeaths}
-                    isLoading={isLoading}
-                    format={formatter}
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'Country',
+        accessor: 'Country',
+      },
+      {
+        id: 'totalCasesCol',
+        Header: 'Total Cases',
+        accessor: 'TotalConfirmed',
+        sortType: 'basic'
+      },
+      {
+        Header: 'New Cases',
+        accessor: 'NewConfirmed',
+        sortType: 'basic',
+        className: 'new-cases-cell'
+      },
+      {
+        Header: 'Total Deaths',
+        accessor: 'TotalDeaths',
+        sortType: 'basic'
+      },
+      {
+        Header: 'New Deaths',
+        accessor: 'NewDeaths',
+        sortType: 'basic'
+      },
+      {
+        Header: 'Recovered',
+        accessor: 'TotalRecovered',
+        sortType: 'basic'
+      },
+    ],
+    []
+  )
 
-                />
-                <HeadDataCard
-                    cardTitle="Recovered"
-                    cardTitleColour="teal"
-                    mainData={summaryData.Global.TotalRecovered}
-                    secondaryData={summaryData.Global.NewRecovered}
-                    isLoading={isLoading}
-                    format={formatter}
-
-                />
-            </div>
-            <div className="grid grid-cols-1">
-                <DisplayTable
-                    cardTitle="Countries"
-                    cardTitleColour="blue"
-                    mainData={summaryData.Countries}
-                    isLoading={isLoading}
-                    format={formatter} />
-            </div>
-        </div>
-    )
+  return (
+    <div className="desktop-wrapper bg-gray-200">
+      <div className="grid grid-cols-6 gap-3">
+        <HeadDataCard
+          cardTitle="Covid Cases"
+          cardTitleColour="blue"
+          mainData={summaryData.Global.TotalConfirmed}
+          secondaryData={summaryData.Global.NewConfirmed}
+          isLoading={isLoading}
+          format={formatter}
+        />
+        <HeadDataCard
+          cardTitle="Deaths"
+          cardTitleColour="red"
+          mainData={summaryData.Global.TotalDeaths}
+          secondaryData={summaryData.Global.NewDeaths}
+          isLoading={isLoading}
+          format={formatter}
+        />
+        <HeadDataCard
+          cardTitle="Recovered"
+          cardTitleColour="teal"
+          mainData={summaryData.Global.TotalRecovered}
+          secondaryData={summaryData.Global.NewRecovered}
+          isLoading={isLoading}
+          format={formatter}
+        />
+      </div>
+      <div className="grid grid-cols-1">
+        <DisplayDataTable
+          columns={columns}
+          data={summaryData.Countries}
+          cardTitle="Daily Country Data"
+          cardTitleColour="blue"
+          isLoading={isLoading}
+          format={formatter}
+        />
+      </div>
+    </div>
+  )
 }
 
 export default DesktopWrapper;
---
+++ src/components/DesktopWrapper/DisplayTable/DisplayDataTable.js
@@ -0,0 +1,176 @@
+import React from 'react'
+import {
+  useTable,
+  useGroupBy,
+  useFilters,
+  useSortBy,
+  useExpanded,
+  usePagination,
+} from 'react-table'
+
+import Loader from '../../../util/Loader/Loader';
+
+const DisplayDataTable = ({ columns, data, cardTitle, isLoading, cardTitleColour, format }) => {
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    prepareRow,
+    page, // Instead of using 'rows', we'll use page,
+    // which has only the rows for the active page
+
+    // The rest of these things are super handy, too ;)
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state: { pageIndex, pageSize },
+  } = useTable(
+    {
+      columns,
+      data,
+      initialState: {
+        pageIndex: 0,
+        sortBy: [
+          {
+            id: 'totalCasesCol',
+            desc: true
+          }
+        ]
+      },
+    },
+    useSortBy,
+    usePagination
+  )
+
+  //Default to loading state
+  let content = (
+    <div className="rounded-sm shadow-md bg-white my-6">
+      <h2 className={`bg-${cardTitleColour}-600 text-white font-semibold text-center p-2`}>
+        {cardTitle}
+      </h2>
+      <div className="p-2">
+        <Loader />
+      </div>
+    </div>
+  )
+
+  if (!isLoading && data) {
+    content = (
+      <div className="rounded-sm shadow-md bg-white">
+        <h2 className={`bg-${cardTitleColour}-600 text-white font-semibold py-1 px-3`}>{cardTitle}</h2>
+
+        {/* Debug pagination
+        
+        <pre>
+          <code>
+            {JSON.stringify(
+              {
+                pageIndex,
+                pageSize,
+                pageCount,
+                canNextPage,
+                canPreviousPage,
+              },
+              null,
+              2
+            )}
+          </code>
+        </pre> */}
+
+        <table  {...getTableProps()} className="table-auto w-full table-fixed">
+          <thead>
+            {headerGroups.map(headerGroup => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map(column => (
+                  // Add the sorting props to control sorting
+                  <th {...column.getHeaderProps(column.getSortByToggleProps())} className="px-4 py-2 text-left">
+                    {column.render('Header')}
+                    <span>
+                      {column.isSorted ? column.isSortedDesc ? ' 🔽' : ' 🔼' : ''}
+                    </span>
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {page.map(
+              (row, i) => {
+                prepareRow(row);
+                return (
+                  <tr {...row.getRowProps()}>
+                    {row.cells.map(cell => {
+                      return (
+                        <td {...cell.getCellProps({
+                          className: cell.column.className
+                        })} className="px-4 py-2 border">
+                          {cell.render('Cell')}
+                        </td>
+                      )
+                    })}
+                  </tr>
+                )
+              }
+            )}
+          </tbody>
+        </table>
+        <div className="pagination">
+          <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+            {'<<'}
+          </button>{' '}
+          <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+            {'<'}
+          </button>{' '}
+          <button onClick={() => nextPage()} disabled={!canNextPage}>
+            {'>'}
+          </button>{' '}
+          <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+            {'>>'}
+          </button>{' '}
+          <span>
+            Page{' '}
+            <strong>
+              {pageIndex + 1} of {pageOptions.length}
+            </strong>{' '}
+          </span>
+          <span>
+            | Go to page:{' '}
+            <input
+              type="number"
+              defaultValue={pageIndex + 1}
+              onChange={e => {
+                const page = e.target.value ? Number(e.target.value) - 1 : 0
+                gotoPage(page)
+              }}
+              style={{ width: '100px' }}
+            />
+          </span>{' '}
+          <select
+            value={pageSize}
+            onChange={e => {
+              setPageSize(Number(e.target.value))
+            }}
+          >
+            {[10, 20, 30, 40, 50].map(pageSize => (
+              <option key={pageSize} value={pageSize}>
+                Show {pageSize}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
+    );
+  }
+  else if (!isLoading && (!data || data === 0)) {
+    content = <p>Could not fetch any data.</p>;
+  }
+  return content;
+}
+
+export default DisplayDataTable;
